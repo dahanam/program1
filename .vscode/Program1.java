e2bbd1d22399bec425240f9c4d827ee48433fa0c
dahanam <dahanamoz@gmail.com>
2023-01-24 22:25:10 -0500

    first commit

---
+++ .vscode/Program1.java
@@ -0,0 +1,9 @@
+public class Program1{
+    public static void main(String[] args){
+        
+        System.out.println("~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Part 5");
+        System.out.println("~~~~~~~~~~~~~~~~~~~");
+    }
+
+}
